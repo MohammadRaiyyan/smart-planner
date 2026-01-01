1451964c40b71bae50b2f8b40a9e9d9147527f89
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-01-01 19:48:15 +0530

    Fix: query zod validator bug

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -11,9 +11,21 @@ type Schema = {
 export const validate =
     (schema: Schema) =>
         (req: Request, _res: Response, next: NextFunction) => {
-            if (schema.body) req.body = schema.body.parse(req.body) as Request["body"];
-            if (schema.query) req.query = schema.query.parse(req.query) as Request["query"];
-            if (schema.params) req.params = schema.params.parse(req.params) as Request["params"];
-            if (schema.headers) req.headers = schema.headers.parse(req.headers) as Request["headers"];
+            if (schema.body) {
+                req.body = schema.body.parse(req.body);
+            }
+
+            if (schema.query) {
+                Object.assign(req.query, schema.query.parse(req.query));
+            }
+
+            if (schema.params) {
+                Object.assign(req.params, schema.params.parse(req.params));
+            }
+
+            if (schema.headers) {
+                Object.assign(req.headers, schema.headers.parse(req.headers));
+            }
+
             next();
         };
